9af9145500fe72e8417f7e2ab13a51d368a7b8fa
deadfish <916548030@qq.com>
2018-07-24 15:00:49 +0800

    实现了串口类

--- uart/uart.py
+++ uart/uart.py
@@ -1,5 +1,6 @@
 __author__ = 'caocongcong'
 import serial
+import serial.tools.list_ports
 from tools.ConstValue import constValue
 
 # 使用串口接受RSII的数据，长度为255
@@ -11,7 +12,7 @@ class uart_communicate():
             print("The Serial port can't find!")
         else:
             for port in port_list:
-            print(port)
+                print(port)
 
         used_port = input("输入串口号:\n")
         try:
@@ -28,9 +29,23 @@ class uart_communicate():
             read_data = self.ser.read()
             self.rssi_data.append(read_data)
 
-
     # 发送数据
-    def send(data):
-        pass
+    def send_data(self, data):
+        data_len = len(data)
+        self.ser.write(bytes(str(data_len),"ascii"))
+        for i in range(data_len):
+            self.ser.write(bytes(str(data[i]),"ascii"))
 
-    # 接受数据
+    # 接收数据
+    def receive(self):
+        data_len =  int.from_bytes(self.ser.read(), byteorder='big')
+        self.receive_data = []
+        for i in range(data_len):
+            self.receive_data.append(self.ser.read())
+
+if __name__ == "__main__":
+    uart_test = uart_communicate()
+    send_data = [0, 1, 0, 1, 0, 1, 0, 1]
+    uart_test.send_data(send_data)
+    uart_test.receive()
+    print(uart_test.receive_data)
